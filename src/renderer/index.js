88d838261c2d39016e9c8db01605bd2ee8fd606c
moon <xiaoxuss@163.com>
2018-01-08 19:13:59 +0800

    Initial commit

---
+++ src/renderer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "antd";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!!!</h1>
+    <Button type="primary">test antd</Button>
+  </div>,
+  document.getElementById("app")
+);
